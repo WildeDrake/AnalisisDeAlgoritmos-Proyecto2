4f7cb01c90bc4cccb8a4d512f5222138df140b9a
dievenegas <diegovenegasanabalon@gmail.com>
2024-06-24 00:32:06 -0400

    comit

--- Item3/Grafo.cpp
+++ Item3/Grafo.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,37 +9,52 @@ using namespace std;
 struct Grafo {
     int V;
     int E;
-    bool** Matriz;
-
+    bool** MatrizAdyecencia;
+    pair<int, int> *Grados;
+    
     Grafo (string nombreArchivo) {
         ifstream file(nombreArchivo);
         string line;
         getline(file, line);
         stringstream ss(line);
         ss >> V >> E;
-        Matriz = new bool*[V];
+        MatrizAdyecencia = new bool*[V];
         for (int i = 0 ; i < V ; i++) {
-            Matriz[i] = new bool[V];
+            MatrizAdyecencia[i] = new bool[V];
             for (int j = 0 ; j < V ; j++) {
-                Matriz[i][j] = false;
+                MatrizAdyecencia[i][j] = false;
             }
         }
         E = 0;
+        Grados = new pair<int, int>[V];
+        for (int i = 0 ; i < V ; i++) {
+            Grados[i] = make_pair(i, 0);
+        }
         while (getline(file, line)) {
             stringstream ss(line);
             int v, w;
             ss >> v >> w;
-            if (Matriz[v-1][w-1] == false ) E++;
-            Matriz[v-1][w-1] = true;
-            Matriz[w-1][v-1] = true;
+            if (MatrizAdyecencia[v-1][w-1] == false ){
+                MatrizAdyecencia[v-1][w-1] = true;
+                MatrizAdyecencia[w-1][v-1] = true;
+                Grados[v-1].second++;
+                Grados[w-1].second++;
+                E++;
+            }
         }
+    }
+
+    void OrdenarGrados() {
+        sort(Grados, Grados + V, [](pair<int, int> a, pair<int, int> b) {
+            return a.second > b.second;
+        });
     }
 
     void imprimir() {
         cout << "Vertices: " << V << "\nAristas: " << E << endl;
         for (int i = 0 ; i < V ; i++) {
             for (int j = 0 ; j < V ; j++) {
-                cout << Matriz[i][j] << " ";
+                cout << MatrizAdyecencia[i][j] << " ";
             }
             cout << endl;
         }
