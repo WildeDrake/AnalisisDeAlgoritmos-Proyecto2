db343180dd81c428e919b59db8ab48a9c7f9f1ad
dievenegas <diegovenegasanabalon@gmail.com>
2024-06-24 22:13:08 -0400

    :,v

--- Item4/Backtracking.cpp
+++ Item4/Backtracking.cpp
@@ -20,14 +20,38 @@ class Backtracking{
         int puntajeSolucion;
 
         void BacktrackingDFS(vector<tupla> A, int k){
-            //Esto sera recursivo.
+            if(k == n){
+                int penalizacion = 0;
+                for(int i = 0; i < m; i++){
+                    int asientos = 0;
+                    for(int j = 0; j < A.size(); j++){
+                        if(A[j].bus == i){
+                            asientos += A[j].miembros;
+                        }
+                    }
+                    if(asientos > P){
+                        penalizacion += asientos - P;
+                    }
+                }
+                if(penalizacion < puntajeSolucion){
+                    puntajeSolucion = penalizacion;
+                    mejorSolucion = A;
+                }
+            }else{
+                for(int i = 0; i < m; i++){
+                    A.push_back({i, k, equipos[k]});
+                    BacktrackingDFS(A, k + 1);
+                    A.pop_back();
+                }
+            }
         }
 
     public:
-        Backtracking(int m, int P, int n, vector<int> equipos){ // no cambiar nada aca, solo es el constructor.
+        Backtracking(int m, int P, int n, vector<int> equipos){
             this->m = m;
             this->P = P;
             this->n = n;
+            this->puntajeSolucion = INT_MAX;
             this->equipos = equipos;
             vector<tupla> A;
             int k = 0;
@@ -49,14 +73,14 @@ class Backtracking{
 
 
 int main(){
-    int m = 4;
-    int P = 50;
-    int n = 6;
-    vector<int> equipos = {40, 30, 40, 50, 10, 30};
+    int m = 4; // Cantidad de buses.
+    int P = 50; // Asientos por bus.
+    int n = 6; // Cantidad de equipos.
+    vector<int> equipos = {40, 20, 40, 50, 10, 30};
     Backtracking b(m, P, n, equipos);
     vector<tupla> mejorSolucion = b.getMejorSolucion();
     for (int i = 0; i < mejorSolucion.size(); ++i) {
-        cout << "Bus: " << mejorSolucion[i].bus << ", Equipo: " << mejorSolucion[i].equipo << ", Miembros: " << mejorSolucion[i].miembros << endl;
+        cout << "Bus: " << mejorSolucion[i].bus+1 << ", Equipo: " << mejorSolucion[i].equipo+1 << ", Miembros: " << mejorSolucion[i].miembros << endl;
     }
     cout << "Penalizacion: " << b.getPuntajeSolucion() << endl;
     return 0;
