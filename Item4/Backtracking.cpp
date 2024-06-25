ff55f6c7355c72a5de65ba927d8782c0bb2af7fb
parallals <frjelvez2021@udec.cl>
2024-06-25 02:11:49 -0400

    mil flags

--- Item4/Backtracking.cpp
+++ Item4/Backtracking.cpp
@@ -15,28 +15,40 @@ class Backtracking{
         int m; // Cantidad de buses.
         int P; // Asientos por bus.
         int n; // Cantidad de equipos.
+        int temp;
         vector<tupla> mejorSolucion;
         int puntajeSolucion;
 
         void BacktrackingDFS(vector<tupla> A, int k, int* Buses, int* equipos, int penalizacion, int suma){
             if(suma == 0){ // Caso base
+                cout<<"Llega al caso base"<<endl;
                 if(penalizacion < puntajeSolucion){
                     puntajeSolucion = penalizacion;
                     mejorSolucion = A;
+                    cout<<"Cambio de sol. óptima"<<endl;
                 }
             } else { // Caso recursivo 
                 for(int i = 0 ; i < m; i++){
+                    cout<<"suma: "<<suma<<endl;
                     if (Buses[i]  > 0){
-                        if(Buses[i] >= equipos[k]){
+                        if(Buses[i] >= equipos[k]){ //Caso en que un bus puede dejar entrear un equipo
+                            cout<<"equipo: "<<k<<" entro al bus "<<i<<endl;
                             A.push_back({i, k, equipos[k]});
                             Buses[i] -= equipos[k];
+
                             BacktrackingDFS(A, k+1, Buses, equipos, penalizacion, suma - equipos[k]);
-                        } else {
+                        } else {    //Caso en que se va a dejar a una fracción del equipo en el bus
+                            cout<<"Buses[i]: "<<Buses[i]<<endl;
+                            cout<<"equipos[k]: "<<equipos[k]<<endl;
+                            cout<<"equipo: "<<k<<" entro medianamente al bus "<<i<<" , entraron "<<Buses[i]<<" personas del equipo"<<endl;
                             equipos[k] -= Buses[i];
                             A.push_back({i, k, Buses[i]});
+                            temp = Buses[i];
                             Buses[i] = 0;
-                            if(equipos[k] > 0) BacktrackingDFS(A, k, Buses, equipos, penalizacion + equipos[k], suma - Buses[i]);
-                            else BacktrackingDFS(A, k+1, Buses, equipos, penalizacion, suma - Buses[i]);
+                            cout<<"equipos[k] restante: "<<equipos[k]<<endl;
+
+                            if(equipos[k] > 0) BacktrackingDFS(A, k, Buses, equipos, penalizacion + equipos[k], suma - temp);
+                            else BacktrackingDFS(A, k+1, Buses, equipos, penalizacion, suma - temp);
                         }
                     }
                 }
